24a7823318be6ca6066a66dc4314547fa9110464
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-09-26 12:42:06 +0200

    created index new and create for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,27 @@
 class GroupsController < ApplicationController
   def index
-    @groups = current_user.groups
+    @groups = policy_scope(Group)
+  end
+
+  def new
+    @group = Group.new
+    authorize @group
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.creator = current_user
+    authorize @group
+    if @group.save!
+      redirect_to groups_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
--- app/policies/group_policy.rb
+++ app/policies/group_policy.rb
@@ -1,8 +1,16 @@
 class GroupPolicy < ApplicationPolicy
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.where(creator: user)
+    end
   end
 end
